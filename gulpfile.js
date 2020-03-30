125d0ff781cd780aeaa7f34b7b76a95ecbc425cc
Param Shah <paramshah09@gmail.com>
2020-03-31 01:16:37 +0530

    complete redo init

--- gulpfile.js
+++ gulpfile.js
@@ -9,16 +9,3 @@ const sync = require('./build/browsersync');
 });
 
 gulp.task('build', ['sass', 'scripts', 'images', 'jekyll-build']);
-
-var deploy = require('gulp-gh-pages');
-
-/**
- * Push build to gh-pages
- */
-gulp.task('deploy', function () {
-  return gulp.src("./build/**/*")
-    .pipe(deploy({
-      remoteUrl: "https://github.com/paramshah10/paramshah10.github.io.git",
-      branch: "master"
-    }))
-});
