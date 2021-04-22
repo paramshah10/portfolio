4a30f6a434c0ab6508a22909877b1b9db82a5e88
Param Shah <paramshah09@gmail.com>
2021-04-21 20:00:22 -0700

    Updates file to support gulp 4

--- build/browsersync.js
+++ build/browsersync.js
@@ -14,34 +14,39 @@ const templatePath = [
 ];
 
 module.exports = gulp => {
+  const reloadBrowser = done => {
+    browserSync.reload();
+    done();
+  };
   // run `jekyll build`
   gulp.task('jekyll-build', done => {
-    return cp.exec(jekyll, ['build'], { stdio: 'inherit' }).on('close', done);
+    return cp.spawn(jekyll, ['build'], { stdio: 'inherit' }).on('close', done);
   });
 
   // run `jekyll build` with _config_dev.yml
   gulp.task('jekyll-dev', done => {
     return cp
-      .exec(jekyll, ['build', '--config', '_config.yml,_config_dev.yml'], {
+      .spawn(jekyll, ['build', '--config', '_config.yml,_config_dev.yml'], {
         stdio: 'inherit',
       })
       .on('close', done);
   });
 
   // Rebuild Jekyll then reload the page
-  gulp.task('jekyll-rebuild', ['jekyll-dev'], () => {
-    browserSync.reload();
-  });
+  gulp.task('jekyll-rebuild', gulp.series(['jekyll-dev', reloadBrowser]));
 
-  gulp.task('serve', ['jekyll-dev'], () => {
-    browserSync.init({
-      server: {
-        baseDir: '_site',
-      },
-    });
+  gulp.task(
+    'serve',
+    gulp.series('jekyll-dev', () => {
+      browserSync.init({
+        server: {
+          baseDir: '_site',
+        },
+      });
 
-    gulp.watch(scssPath, ['sass', browserSync.reload]);
-    gulp.watch(jsPath, ['scripts', browserSync.reload]);
-    gulp.watch(templatePath, ['jekyll-rebuild']);
-  });
+      gulp.watch(scssPath, gulp.series(['sass', reloadBrowser]));
+      gulp.watch(jsPath, gulp.series(['scripts', reloadBrowser]));
+      gulp.watch(templatePath, gulp.task('jekyll-rebuild'));
+    })
+  );
 };
